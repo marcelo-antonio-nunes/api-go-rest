7d955d08e6d14fb20d1f555e72a4fcf8aab56494
marcelo-antonio-nunes <marcelo197519@gmail.com>
2022-11-06 15:58:56 -0300

    finalizado

--- routes/routes.go
+++ routes/routes.go
@@ -7,14 +7,18 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/marcelo-antonio-nunes/api-go-rest.git/controllers"
+	"github.com/marcelo-antonio-nunes/api-go-rest.git/controllers/middleware"
 )
 
 func HandleResquest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
-	r.HandleFunc("/api/personalidade", controllers.TodasPersonalidades).Methods("Get")
+	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
 	r.HandleFunc("/api/personalidades", controllers.CriaUmaNovaPersonalidade).Methods("Post")
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
+	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPersonalidade).Methods("Put")
 
 	fmt.Println("Iniciando Servidor Rest com Go")
 	log.Fatal((http.ListenAndServe(":8080", r)))
